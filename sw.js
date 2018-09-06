b42afc37a9c810aa6e1bec1562d6249f3933963d
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2018-09-06 13:16:08 -0500

    Updated necessary changes

--- sw.js
+++ sw.js
@@ -29,7 +29,29 @@ self.addEventListener('install', function(e) {
     console.log(event.request.url);
     event.respondWith(
         caches.match(event.request).then(function(response) {
-          return response || fetch(event.request);
+          if (response){
+              return response;
+          }
+
+        //Clone the response
+        var fetchRequest = event.request.clone();
+
+        return fetch(fetchRequest).then(
+            function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+
+            var responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+                .then(function(cache) {
+                cache.put(event.request, responseToCache);
+                });
+
+                return response;
+                }
+            );
         })
     );   
 });
