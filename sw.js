b02784a1d04b1192a47fb66ac0b6a52c08e8f91f
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2018-09-06 13:41:00 -0500

    Fixed service worker

--- sw.js
+++ sw.js
@@ -23,35 +23,36 @@ self.addEventListener('install', function(e) {
         ]);
       })
     );
-   });
+});
 
-   self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
+self.addEventListener('fetch', function(event) {
     event.respondWith(
-        caches.match(event.request).then(function(response) {
-          if (response){
-              return response;
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
           }
-
-        //Clone the response
-        var fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(
+  
+          var fetchRequest = event.request.clone();
+  
+          return fetch(fetchRequest).then(
             function(response) {
-            // Check if we received a valid response
-            if(!response || response.status !== 200 || response.type !== 'basic') {
+              // Check if we received a valid response
+              if(!response || response.status !== 200 || response.type !== 'basic') {
+                return response;
+              }
+  
+              var responseToCache = response.clone();
+  
+              caches.open('restaurant-reviews')
+                .then(function(cache) {
+                  cache.put(event.request, responseToCache);
+                });
+  
               return response;
             }
-
-            var responseToCache = response.clone();
-            caches.open(CACHE_NAME)
-                .then(function(cache) {
-                cache.put(event.request, responseToCache);
-                });
-
-                return response;
-                }
-            );
+          );
         })
-    );   
-});
+      );
+  });
